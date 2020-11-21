055f76e448ab2e2968bae4f11a1422d30ea41017
ipal2006 <72303965+ipal2006@users.noreply.github.com>
2020-11-21 12:16:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+song = "";
+song2= "";
+
+
+function preload()
+{
+song = loadSound("music.mp3");
+song2 = loadSound("music2.mp3");
+}
+function play()
+{
+    song.play();
+}
+function setup()
+{
+canvas = createCanvas(600,500);
+canvas.center();
+video = createCapture(VIDEO);
+video.hide();
+
+}
+
+
+function draw()
+{
+image(video,0,0,600,500);
+}
+
